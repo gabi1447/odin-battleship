d48ceed698c76f59e5673137931cce297add023f
Gabriel <gabrielpachecotec@gmail.com>
2025-03-06 10:34:46 +0000

    Make tests more self-contained

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,10 +1,15 @@
-import { ship } from "./ship.js";
+import { Ship } from "./ship.js";
 
 describe("ship factory", () => {
-    const testShip = ship(4);
+    let testShip;
+    const shipLength = 4;
+
+    beforeEach(() => {
+        testShip = Ship(shipLength);
+    });
 
     test("length of ship", () => {
-        expect(testShip.length).toBe(4);
+        expect(testShip.length).toBe(shipLength);
     });
 
     test("ship is not sunk", () => {
@@ -12,21 +17,21 @@ describe("ship factory", () => {
     });
 
     test("ship is sunk", () => {
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < shipLength; i++) {
             testShip.hit();
         }
         expect(testShip.isSunk()).toBeTruthy();
     });
 
-    test("hit doesn't increment if hits is equal to length", () => {
-        const ship2 = ship(1);
+    test("hit doesn't increment if hits is equal to ship length", () => {
+        const ship2 = Ship(1);
         ship2.hit();
         ship2.hit();
         expect(ship2.hits).toBe(1);
     });
 
     test("hit increments hits property", () => {
-        const ship3 = ship(1);
+        const ship3 = Ship(1);
         ship3.hit();
         expect(ship3.hits).toBe(1);
     });
