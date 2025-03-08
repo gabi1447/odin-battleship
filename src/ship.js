980c868bc042aeb7dcb8a43e85464c69740f7ff0
Gabriel <gabrielpachecotec@gmail.com>
2025-03-08 10:49:16 +0000

    Write generateCoordinates and declare getter and setter for coordinates list instance variable

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,6 @@
 export function Ship(l) {
     const length = l;
+    let coordinatesList;
     let hits = 0;
 
     function hit() {
@@ -13,12 +14,38 @@ export function Ship(l) {
         return hits === length;
     }
 
+    function generateCoordinates(start, end) {
+        let arrayOfCoordinates = [];
+        if (start[0] === end[0]) {
+            const s = start[1];
+            const e = end[1];
+            for (let i = s; i <= e; i++) {
+                arrayOfCoordinates.push([start[0], i]);
+            }
+        } else if (start[1] === end[1]) {
+            const s = start[0];
+            const e = end[0];
+            for (let i = s; i <= e; i++) {
+                arrayOfCoordinates.push([i, start[1]]);
+            }
+        }
+
+        return arrayOfCoordinates;
+    }
+
     return {
         length,
         get hits() {
             return hits;
         },
+        get coordinatesList() {
+            return coordinatesList;
+        },
+        set coordinatesList(arrayOfCoordinates) {
+            coordinatesList = arrayOfCoordinates;
+        },
         isSunk,
         hit,
+        generateCoordinates,
     };
 }
