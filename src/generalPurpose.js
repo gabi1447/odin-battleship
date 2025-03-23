753d5e27fb20963b8b6158429cdcff313470f5dc
Gabriel <gabrielpachecotec@gmail.com>
2025-03-23 08:59:47 +0000

    Add general purpose functions module

---
+++ src/generalPurpose.js
@@ -0,0 +1,33 @@
+export function oneDigitNumberToNumberWord(integer) {
+    const numberMapping = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    };
+
+    return numberMapping[integer];
+}
+
+export function numberTextToInt(numberText) {
+    const numberTextMapping = {
+        zero: 0,
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4,
+        five: 5,
+        six: 6,
+        seven: 7,
+        eight: 8,
+        nine: 9,
+    };
+
+    return numberTextMapping[numberText];
+}
